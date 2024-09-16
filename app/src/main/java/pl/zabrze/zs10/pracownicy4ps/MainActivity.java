5329f2e25041e60ed3f1f00aad655044ce4fd9c2
markup54 <marzenakupka@tlen.pl>
2024-09-16 13:35:37 +0200

    wyswietlanie pracownikow

--- app/src/main/java/pl/zabrze/zs10/pracownicy4ps/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/pracownicy4ps/MainActivity.java
@@ -3,11 +3,23 @@ package pl.zabrze.zs10.pracownicy4ps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Pracownik> pracownicy = new ArrayList<>();
+    TextView textViewImie;
+    TextView textViewStanowisko;
+    ImageView imageViewStaz;
+    Button buttonDalej;
+    Button buttonWstecz;
+    int aktualnyNrPracownika = 0;
+    Button buttonUsun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +27,64 @@ public class MainActivity extends AppCompatActivity {
         pracownicy.add(new Pracownik("Jaś","Tester",0));
         pracownicy.add(new Pracownik("Asia","Programista",2));
         pracownicy.add(new Pracownik("Isia","Informatyk",1));
+        textViewImie = findViewById(R.id.textView);
+        textViewStanowisko = findViewById(R.id.textView2);
+        imageViewStaz = findViewById(R.id.imageView);
+
+        wyswietlPracownika(aktualnyNrPracownika);
+        buttonDalej = findViewById(R.id.button2);
+        buttonDalej.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        aktualnyNrPracownika++;
+                        if(aktualnyNrPracownika == pracownicy.size()){
+                            aktualnyNrPracownika = 0;
+                        }
+                        wyswietlPracownika(aktualnyNrPracownika);
+                    }
+                }
+        );
+        buttonWstecz = findViewById(R.id.button);
+        buttonWstecz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        aktualnyNrPracownika --;
+                        if(aktualnyNrPracownika<0) {
+                            aktualnyNrPracownika = pracownicy.size() - 1;
+                        }
+                        wyswietlPracownika(aktualnyNrPracownika);
+                    }
+                }
+        );
+        buttonUsun = findViewById(R.id.button3);
+        buttonUsun.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(pracownicy.size()>1) {
+                            pracownicy.remove(aktualnyNrPracownika);
+                            aktualnyNrPracownika = 0;
+                            wyswietlPracownika(aktualnyNrPracownika);
+                        }
+                        else{
+
+                            Toast.makeText(MainActivity.this,
+                                    "To jest ostatni pracownik:)",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+    }
 
 
-
+    private void wyswietlPracownika (int i){
+        textViewImie.setText(pracownicy.get(i).getImie());
+        textViewStanowisko.setText(pracownicy.get(i).getStanowisko());
+        int[] idObrazkow = new int[]{R.drawable.junior, R.drawable.middle,R.drawable.senior};
+        imageViewStaz.setImageResource(idObrazkow[pracownicy.get(i).getStaz()]);
     }
 }
